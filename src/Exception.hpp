d54925a29ae3601e5023c76ddc5d75200264f660
christojeffrey <71057204+christojeffrey@users.noreply.github.com>
2022-03-18 13:12:24 +0700

    adding exception on exception hpp and gamestate

--- src/Exception.hpp
+++ src/Exception.hpp
@@ -1,5 +1,5 @@
-#ifndef __BASE_EXCEPTION_HPP__
-#define __BASE_EXCEPTION_HPP__
+#ifndef __EXCEPTION_HPP__
+#define __EXCEPTION_HPP__
 
 #include <iostream>
 using namespace std;
@@ -23,45 +23,69 @@ class ExampleException : public BaseException {
 /* for craftTable */
 class craftTableIsNotEmptyException : public BaseException { // happened when the craft table at c_id is not empty when it supposed to be empty
     public:
-        void printMessage();
+        void printMessage(){
+            cout << "craft table is not empty" << endl;
+        }
 };
 
 class craftTableIsEmptyException : public BaseException { // happened when the craft table at c_id is empty when it supposed to not be empty
     public:
-        void printMessage();
+        void printMessage(){
+            cout << "craft table is empty" << endl;
+        }
 };
 class craftTableDoesntMatchRecipeException : public BaseException { 
     // happened when trying to craft on craft table but doesnt match any recipe
     public:
-        void printMessage();
+        void printMessage(){
+            cout << "craft table does not match any recipe" << endl;
+        }
 };
 
 class CIDNotValid : public BaseException { 
     // happened when trying to craft on craft table but doesnt match any recipe
     public:
-        void printMessage();
+        void printMessage(){
+            cout << "C id is not a valid" << endl;
+        }
 };
 
 /* for inventory */
 class inventoryIsFullException : public BaseException { // happened when the inventory is full
     public:
-        void printMessage();
+        void printMessage(){
+            cout << "inventory is full" << endl;
+        }
 };
 class inventoryIdIsEmptyException : public BaseException{
     // happened when the inventory on I_id is empty
     public:
-        void printMessage();
+        void printMessage(){
+            cout << "I id is empty" << endl;
+        }
 };
 class inventoryIdIsInsufficientException : public BaseException{
     // happened when the inventory on I_id is insufficient
     public:
-        void printMessage();
+        void printMessage(){
+            cout << "I id has insufficient quantity" << endl;
+        }
 };
 
 class inventoryIdIsNotToolException : public BaseException{
     // happened when inventory on I_id is not a tool when it is supposed to be tool
     public:
-        void printMessage();
+        void printMessage(){
+            cout << "I id give is not a tool. it's supposed to be a tool" << endl;
+        }
+};
+
+class inventoryItemNameIsNotFoundException : public BaseException{
+    // happened when inventory on I_id is not a tool when it is supposed to be tool
+    public:
+        void printMessage(){
+            cout<< "Nama item tidak ditemukan" << endl;
+        }
 };
 
 #endif
--- src/GameState.cpp
+++ src/GameState.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #include "CraftTable.hpp"
 #include "Inventory.hpp"
 #include "Item.hpp"
-
+#include "Exception.hpp"
 GameState::GameState(list<Item> _legalItem, list<CraftTable> _legalRecipe){
     this->legalItem = _legalItem;
     this->legalRecipe = _legalRecipe;
@@ -30,17 +30,19 @@ void GameState::GIVE(string item_name, int qty){
     if(found){
         try{
             this->inventory.add(new NonTool(item_name,type,qty));
-        } catch(...){
+        } catch(BaseException *e){
+            e->printMessage();
             //kalau inventory penuh
         }
     }else{
-        throw "Nama item tidak ditemukan";
+        throw new inventoryItemNameIsNotFoundException;
     }
 }
 void GameState::DISCARD(string I_id, int qty){
     try{
         this->inventory.substract(I_id,qty);
-    }catch(...){
+    }catch(BaseException *e){
+        e->printMessage();
         //kalau I_id tersebut kosong atau kurang
     }
 }
@@ -52,11 +54,13 @@ void GameState::MOVE(string I_id, int N, vector<string> C_id){
             for(iterasi vector c_id){
                 this->craftTable.add(itemnyaapa,c_id);
             }
-        }catch(...){
+        }catch(BaseException *e){
+        e->printMessage();
             //kalau di C_id sudah ada yg menempati
             this->inventory.add(itemnyaapa,N);
         }
-    }catch(...){
+    }catch(BaseException *e){
+        e->printMessage();
         //kalau jumlah di inventory kurang
     }
 }
@@ -65,21 +69,24 @@ void GameState::MOVE(string C_id, string I_id){
         Item itemnyaapa=0;
         this->craftTable.substract(C_id);
         this->inventory.add(itemnyaapa,I_id);
-    }catch(...){
+    }catch(BaseException *e){
+        e->printMessage();
         //kalau di craft kosong atau inventory penuh
     }
 }
 void GameState::USE(string I_id){
     try{
         this->inventory.use(I_id); //sudah termasuk menghilangkan bila durability 0
-    }catch(...){
+    }catch(BaseException *e){
+        e->printMessage();
         //kalau I_id bukan tool
     }
 }
 void GameState::CRAFT(){
     try{
         this->craftTable.make();
-    }catch(...){ 
+    }catch(BaseException *e){
+        e->printMessage();
         //bila tidak sesuai dengan resep
     }
 }
