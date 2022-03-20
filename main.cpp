02e3df1f75a9704961379060b34ccdaab411f4ca
christojeffrey <71057204+christojeffrey@users.noreply.github.com>
2022-03-21 01:34:47 +0700

    mencoba bikin recipe

--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@
 #include <bits/stdc++.h>
 #include <vector>
 #include <list>
-
+#include <sstream>
+#include <map>
 #include "src/Item.hpp"
 #include "src/Tool.hpp"
 #include "src/NonTool.hpp"
-
+#include "src/Recipe.hpp"
 using namespace std;
 
 int main() {
@@ -22,6 +23,10 @@ int main() {
 
   // CREATE LIST OF LEGAL ITEM
   list<Item*> legalItem;
+
+  //buat ngebantu bikin recipe, aku butuh sebuah item, ketika diketahui nama item.
+  map<string, Item> ItemNameToItemObject; 
+  map<string, bool> isItemATool;
 
   //read from config 
   ifstream itemConfigFile(itemConfigPath);
@@ -60,24 +65,39 @@ int main() {
     // cout << "\ttoolnontool = " << toolnontool << endl;
     /* FOR DEBUGGING PURPOSE */
 
+   
+
+
     //add to legal list
     if(toolnontool == "TOOL"){
       //tambahkan tool ke legal item
       Tool* temp = new Tool(stoi(id) ,name, 10);
       // temp->printInfo();
       legalItem.push_back(temp);
+
+      //add to recipe helper
+      //ItemNameToItemObject.insert(pair<string,Item> (name,*temp));
+      //isItemATool.insert(pair<string,bool> (name, true));
     }
     else{
       //tambahakn nontool ke legal item
       NonTool* temp = new NonTool(stoi(id), name, nonToolClass,1); // quantity disini harusnya gaperna disentuh. aku kasih 1. kenapa gk dikasih 0 aja? takutnya nti bikin behaviour yg aneh
       legalItem.push_back(temp);
+
+            //add to recipe helper
+
+      //ItemNameToItemObject.insert(pair<string,Item> (name,*temp));
+      //isItemATool.insert(pair<string,bool> (name, false));
+
     }
   }
 
-  list<Item*>::iterator it = legalItem.begin();
-  for(it = legalItem.begin(); it != legalItem.end();it++){ //
-    (*it)->printInfo();
-  }
+  /* FOR DEBUGGING PURPOSE */
+  // list<Item*>::iterator it = legalItem.begin();
+  // for(it = legalItem.begin(); it != legalItem.end();it++){ //
+  //   (*it)->printInfo();
+  // }
+  /* FOR DEBUGGING PURPOSE */
 
   // read recipes
   for (const auto &entry :filesystem::directory_iterator(configPath + "/recipe")) {
@@ -85,11 +105,63 @@ int main() {
     
     // create list of legal recipe
     ifstream eachRecipeFile(entry.path());
+
+    //changing text inside into one long string
+    string currentRecipeString;
     for (string line; getline(eachRecipeFile, line);) {
       //each line inside a recipe
-      cout << "\t" << line << endl;
-      
+      //cout << "\t" << line << endl;
+      currentRecipeString.append(line);
+      currentRecipeString.append(" ");
     }
+    cout <<"\t" << currentRecipeString << endl;
+    
+
+    //spliting long string into each variable needed to make a recipe
+    string currentWordRecipe;
+    int count = 0;
+    int row = 0;
+    int col = 0; 
+    vector<string> eachRecipe;
+    string itemName;
+    int resultquantity = 0;
+
+    stringstream ssrecipe(currentRecipeString);
+
+    //assigning into the correct variable
+    while(ssrecipe >> currentWordRecipe) {
+      count++;
+      cout <<"\t\t" << count << ". " << currentWordRecipe << endl;
+      if(count == 1){
+        row = stoi(currentWordRecipe);
+      }
+      else if(count == 2){ //
+        col = stoi(currentWordRecipe);
+      }
+      else if(count > 2 && count <= ((row*col) + 2)){
+        //add to vector recipe
+        eachRecipe.push_back(currentWordRecipe);
+      }
+      else if(count == ((row*col) + 3)){ //
+        itemName = currentWordRecipe;
+        cout << "INI ITEM NAMENYA " << currentWordRecipe << endl;
+      }
+      else if(count == ((row*col) + 4)){ //
+        resultquantity = stoi(currentWordRecipe);
+        cout << "INI jumlah yg kebikin " << currentWordRecipe << endl;
+      }
+    }
+    //creating Recipe Object
+    // if(isItemATool[itemName]){
+    //   //recipe made for tool
+
+    //   int tempid = ItemNameToItemObject[itemName].getID();
+    //   string tempname = ItemNameToItemObject[itemName].getName();
+    //   int tempdurability = ItemNameToItemObject[itemName].getDurability();
+      //Recipe temp = Recipe(row, col, eachRecipe, Tool(tempid, tempname, tempdurability),resultquantity);
+      // Recipe temp2 = Recipe(1, 1, vector<string>(3,"test"),Tool(tempid, tempname, tempdurability),1);
+
+    // }
   }
 
 
